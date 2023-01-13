a4c92c465db876eaddfc79a2e59976e8c0af4301
Dino Čeliković <dino.celikovic@hycu.com>
2023-01-13 13:51:13 +0100

    Added find_grizzly_files function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-'''
-MAIN FILE
-'''
+"""
+Author: Dino Celikovic
+Script for automating transfer of build files to remote HYCU backup controller via SFTP protocol
+"""
 from ftplib import FTP
 import pysftp
 import os
@@ -8,22 +9,49 @@ import sys
 
 HOST = "dinoce-ghoul2"
 USER = "root"
-PASS = "Nux2017."
+PASS = "" # Define password before using script (PASS is empty for Git security purposes for now (arguments will be implemented))
 
-root_dir = 'C:\\'
+PROJECT_ROOT = ""
 
-def copy_local_files():
-    for i, (folder, subfolders, files) in enumerate(os.walk(root_dir)):
-        if(i<=1):
-            print(folder)
-        else:
-            break
+
+LIB_FOLDER_PATH = ""
+DATABASE_FOLDER_PATH = ""
+WWW_FOLDER_PATH = ""
+
+
+ROOT_DIRECTORY = 'C:\\Users'
+HOME_DIRECTORY = os.path.expanduser( '~' )
+
+
+def find_grizzly_files():
+    global PROJECT_ROOT, LIB_FOLDER_PATH, DATABASE_FOLDER_PATH, WWW_FOLDER_PATH
+    for root, dirs, files in os.walk(HOME_DIRECTORY): # Assumes project is somewhere in the home directory (needs improvement)
+        for dir in dirs:
+            if dir.strip() == "enterprise-cloud" and PROJECT_ROOT.strip() == "": # Searching only up to enterprise cloud to shorten search time
+                PROJECT_ROOT = os.path.join(root, dir) + "\\Virtual Appliance\\opt\\grizzly"
+                LIB_FOLDER_PATH = PROJECT_ROOT + "\\lib"
+                DATABASE_FOLDER_PATH = PROJECT_ROOT + "\\database"
+                WWW_FOLDER_PATH = PROJECT_ROOT + "\\www"
+                #TODO: separate folder paths may not be necessary, just project root, can add specific paths when copying to remote
+                '''
+                print ("Root: " + PROJECT_ROOT + "\n")
+                print ("Lib: " + LIB_FOLDER_PATH + "\n")
+                print ("DB: " + DATABASE_FOLDER_PATH + "\n")
+                print ("www: " + WWW_FOLDER_PATH + "\n")
+                '''
+                break
+
+def copy_local_to_remote(sftp, localpath, remotepath):
+    pass
+
 
 def upload_to_remote():
     with pysftp.Connection(HOST, username=USER, password=PASS) as sftp:
-        print(sftp.pwd)
+        sftp.execute("rm -rf /opt/grizzly/lib/*")
+        sftp.put()
 
 
 if __name__ == "__main__":
-    copy_local_files()
+    print("Found project files")
+    find_grizzly_files()
     #upload_to_remote()
