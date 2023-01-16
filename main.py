0c66b1cbbc6dca74f08e484013d6b3ad9aaa7e45
Dino Čeliković <dino.celikovic@hycu.com>
2023-01-16 15:21:14 +0100

    Fixed file transfer to remote, TODO: implement recursive directory transfer

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import sys
 
 HOST = "dinoce-ghoul2"
 USER = "root"
-PASS = "" # Define password before using script (PASS is empty for Git security purposes for now (arguments will be implemented))
+PASS = "Nux2017." # Define password before using script (PASS is empty for Git security purposes for now (arguments will be implemented))
 
 PROJECT_ROOT = ""
 
@@ -38,8 +38,8 @@ def find_grizzly_files():
             if dir.strip() == "enterprise-cloud" and PROJECT_ROOT.strip() == "": # Searching only up to enterprise cloud to shorten search time
                 PROJECT_ROOT = os.path.join(root, dir) + "\\Virtual Appliance\\opt\\grizzly"
                 LIB_FOLDER_PATH = PROJECT_ROOT + "\\lib\\"
-                DATABASE_FOLDER_PATH = PROJECT_ROOT + "\\database"
-                WWW_FOLDER_PATH = PROJECT_ROOT + "\\www"
+                DATABASE_FOLDER_PATH = PROJECT_ROOT + "\\database\\"
+                WWW_FOLDER_PATH = PROJECT_ROOT + "\\www\\"
                 #TODO: separate folder paths may not be necessary, just project root, can add specific paths when copying to remote
                 '''
                 print ("Root: " + PROJECT_ROOT + "\n")
@@ -47,37 +47,49 @@ def find_grizzly_files():
                 print ("DB: " + DATABASE_FOLDER_PATH + "\n")
                 print ("www: " + WWW_FOLDER_PATH + "\n")
                 '''
-                print ("Lib: " + LIB_FOLDER_PATH + "\n")
                 break
 
 def replace_remote_with_local(sftp, localroot, remoteroot):
     pass
 
 
+def rm(sftp, path):
+    files = sftp.listdir(path)
+    for f in files:
+        filepath = path + "/" + f
+        try:
+            sftp.remove(filepath)
+        except IOError:
+            rm(sftp, filepath)
+
+    sftp.rmdir(path)
+
+def upload_dir(sftp, path):
+    pass
+
 def upload_to_remote():
     global PROJECT_ROOT, LIB_FOLDER_PATH, DATABASE_FOLDER_PATH, WWW_FOLDER_PATH
     with pysftp.Connection(HOST, username=USER, password=PASS) as sftp:
-        '''
-        if sftp.exists("/opt/grizzly/lib"):
-            sys.stdout.write("Deleting all lib files...\n")
-            sftp.execute("rm -rf /opt/grizzly/lib")
-        '''
-        # TODO: upload folder from local to remote with put_d (if possible, else file by file)
         print("Uploading files to remote...")
-        #sftp.put_d(LIB_FOLDER_PATH, "/opt/grizzly/")
-        if not sftp.exists("/opt/grizzly/lib"):
-            sftp.mkdir("/opt/grizzly/lib/")
-        for file in os.listdir(LIB_FOLDER_PATH):
-            local_file = LIB_FOLDER_PATH + file
-            remote_file = "/opt/grizzly/lib/" + file
-            print(file)
-            print(local_file)
-            print(remote_file + "\n")
-            if(sftp.exists("/opt/grizzly/lib/" + file)):
-                sftp.remove("/opt/grizzly/lib/" + file)
-            sftp.put(localpath=local_file, remotepath=remote_file)
-            #upper code works but it doesn't copy fips folders (because they are folders)
-            #sftp.put_r(LIB_FOLDER_PATH + , "/opt/grizzly/lib/")
+        #removes dir at remote if it exists
+        if sftp.exists("/opt/grizzly/lib/"):
+            rm(sftp, "/opt/grizzly/lib")
+        sftp.mkdir("/opt/grizzly/lib/")
+        for fso in os.listdir(LIB_FOLDER_PATH):
+            local_path = LIB_FOLDER_PATH + fso
+            remote_path = "/opt/grizzly/lib/" + fso
+            #print(fso)
+            #print(local_path)
+            #print(remote_path + "\n")
+            if os.path.isfile(local_path):
+                sftp.put(localpath=local_path, remotepath=remote_path)
+            elif os.path.isdir(local_path):
+                pass
+            '''
+            code above works but it doesn't copy directories, 
+            need to implement upload_dir function to upload directory and it's contents recursively
+            '''
+
 
 
 if __name__ == "__main__":
